b2ab9302d1f1f537e943d77950fc33660acb77f6
Ax <axdeklerk@gmail.com>
2025-01-20 08:52:43 +0000

    feat added tests for player moves choices

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -24,4 +24,16 @@ describe("game object contains correct keys", () => {
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
     });
+
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+
+    test("choices keys contain the correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
