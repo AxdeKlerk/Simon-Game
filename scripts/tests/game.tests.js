8590d25b2956892799531af88796758bac3c305a
Ax <axdeklerk@gmail.com>
2025-01-20 07:04:53 +0000

    feat: game object created

--- scripts/tests/game.tests.js
+++ scripts/tests/game.tests.js
@@ -5,16 +5,18 @@
 const { beforeAll, test, expect } = require("@jest/globals");
 const { describe } = require("yargs");
 
+const {game} = require("../game");
+
 beforeAll (() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileCOntents);
     document.close();
-})
+});
 
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("socre" in game).toBe(true);
-    })
-})
+    });
+});
